6045cb6bdaa806e90ad6558102ad842616bba4e6
Régis <xp@regisroy.net>
2019-06-25 23:03:14 +0200

    cucumber : check config

---
+++ src/test/java/Story1Stepdefs.java
@@ -0,0 +1,31 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+import static org.junit.Assert.*;
+
+public class Story1Stepdefs {
+
+
+//    public Story1Stepdefs() {
+
+    @Given("^today is Sunday$")
+    public void today_is_Sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I ask whether it's Friday yet$")
+    public void i_ask_whether_it_s_Friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String arg1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+//    }
+}
---
+++ src/test/java/kata/bankaccount/functional/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package kata.bankaccount.functional;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+//@CucumberOptions(format={"pretty", "html:target/cucumber/html", "json:target/cucumber/json/cucumber.json"})
+public class RunCucumberTest {
+
+}
