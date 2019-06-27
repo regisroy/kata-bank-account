21f10527e22a585702590d4f5d462de17e8e35a1
Régis Roy <regis.roy@generativeobjects.com>
2019-06-27 12:01:14 +0200

    run Cucumber tests with maven

--- src/test/java/kata/bankaccount/functional/RunFunctionalCucumberTests.java
+++ src/test/java/kata/bankaccount/functional/RunFunctionalCucumberTests.java
@@ -1,10 +1,11 @@
 package kata.bankaccount.functional;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-//@CucumberOptions(format={"pretty", "html:target/cucumber/html", "json:target/cucumber/json/cucumber.json"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber.json"})
 public class RunFunctionalCucumberTests {
 
 }
